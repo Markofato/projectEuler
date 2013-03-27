8226aa0549fa290080fc2b5eef9d58b9491ab3db
Mark Webber <mark.d.webber@gmail.com>
2013-03-27 17:48:47 +0800

    changes to break up search. doesn't account for all possibilities.

--- NotCompleted/NCP240 Top Dice.py
+++ NotCompleted/NCP240 Top Dice.py
@@ -1,4 +1,11 @@
-res = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+#[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] <- complete range
+#[1, 2, 3, 4, 5, 6, 7] <- range for gLterms
+#{1: 1, 2: 1024, 3: 59049, 4: 1048576, 5: 9765625, 6: 60466176, 7: 282475249} <- dict
+#3,833,759,992,447,475,122,176L <- total number of pos
+#3.834 * (10 ** 21)
+# find where top 10 == 70. return gLterm if gLterm != 1;
+#                gLterm is key for it's multiplier of outcomes.
 
 '''
 def start():
@@ -29,17 +36,24 @@ def start():
                                                                                     if topTenTotal == 70:
                                                                                         num+=1
                                                                                         if num % 100000==0:print("Num = {}. a = {}. j = {}. o = {}.".format(num,a,j,o))
-                                                                                    
+
 
 
 start()
-'''
+'''  # <-- lazy equation zzzz.
 
-# find where top 10 == 70. for each grab bot term and put trough dict
+# [462, 97713, 4772504]
 
-def botTenGen(res, n):
-    numPos = 0
-    res = res[:n]
+# finds the greatest lowest term for the top ten. top == 70
+#   ex: topTen = [-> 1 <-, 1, 1, 1, 6, 12, 12, 12, 12, 12] -> 4(1) + 6 + 5(12) == 70
+#   ex: topTen = [-> 7 <-, 7, 7, 7, 7, 7, 7, 7, 7, 7] -> 10(7) == top.
+#                   There is no way gLterm can be 8 for these conditions.
+def findHighestTerm(res, top):
+    #top == 70
+    #res == numbers 1 to 12
+    #   REQUIRES DEBUGGING. THIS SHIT GARNA' TAKE AWHILE
+    #   returns greatest lowest term of 10
+    gLowestTerm = 0
     for a in res:
         for b in res:
             for c in res:
@@ -50,17 +64,106 @@ def botTenGen(res, n):
                                 for h in res:
                                     for i in res:
                                         for j in res:
+                                            listofTerms = [a,b,c,d,e,f,g,h,i,j]
+                                            if sum(listofTerms) == top:
+                                                lowestTerm = sorted(listofTerms)[0]
+                                                if gLowestTerm < lowestTerm:
+                                                    gLowestTerm = lowestTerm
+    return gLowestTerm # confirms GLowest = 7
+
+
+# (maxRange of for loop) ** (number of for loops)
+def botTenGen(numList, limit):
+    numList = numList[:limit]
+    numPos = 0
+    for a in numList:
+        for b in numList:
+            for c in numList:
+                for d in numList:
+                    for e in numList:
+                        for f in numList:
+                            for g in numList:
+                                for h in numList:
+                                    for i in numList:
+                                        for j in numList:
                                             numPos += 1
+    #print(a,b,c,d,e,f,g,h,i,j)
     return numPos
 
-# n = topList[0]
 
-def constDictGivenLowest(res, n):
-    posLower = {}
-    res = res[:n]
-    for limit in res:
-        posLower[limit] = botTenGen(res, limit)
-    return posLower
+#Only needs to be done once. DONE. refer to dict at top.
+def constDictGivenLowest():
+    numsBotGen = [1, 2, 3, 4, 5, 6, 7]
+    combBotTen = {}
+    for limit in numsBotGen:
+        combBotTen[limit] = botTenGen(numsBotGen, limit)
+    return combBotTen
 
-print(constDictGivenLowest(res, 3))
-#print(botTenGen(res, 3))
+
+def outComesWithgLterm1():
+    """ when gL = 1, botTen all = 1 and since;
+    12,12,12,12,12,6,1,1,1,1 = 6 loops used; it can be repeated to 9 loops for gL = 1
+    finds number of possible answers which satisfy:
+    botTen = [1, 1, ... , 1] and topTen[:10 - n] = [1, 1, 1, 1, x, x, ... , x]
+                                                 = [1, 1, 1, x, x, x, ... , x]
+                                                 = [1, 1, x, x, x, x, ... , x]
+                                                 = [1, x, x, x, x, x, ... , x]
+    """
+    total = []
+    loops = [6,7,8,9]
+
+    for n in loops:
+        total.append(loopFunc(1,12, n))
+    return total
+
+
+def getNumList(rangeMin, rangeMax, nLoops):
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    nums = nums[rangeMin - 1:rangeMax]
+    numList = []
+    for n in range(10, 0, -1):
+        if n > nLoops:
+            numList.append([1])
+        else:
+            numList.append(nums)
+    return numList
+
+
+#input range and number of loops: ([1-12], [1-12], [1-10])
+def loopFunc(rangeMin, rangeMax, nLoops):
+    numList = getNumList(rangeMin, rangeMax, nLoops)
+    numPos = 0
+    for a in numList[0]:
+        for b in numList[1]:
+            for c in numList[2]:
+                for d in numList[3]:
+                    for e in numList[4]:
+                        for f in numList[5]:
+                            for g in numList[6]:
+                                for h in numList[7]:
+                                    for i in numList[8]:
+                                        for j in numList[9]:
+                                            total = sum([a,b,c,d,e,f,g,h,i,j])
+                                            if total == 70:
+                                                numPos += 1
+
+    return numPos
+
+
+#wrapper
+def run():
+    total = []  # list to break up each section.
+    gLTotals = []
+    combBotTen = {1: 1, 2: 1024, 3: 59049, 4: 1048576, 5: 9765625, 6: 60466176, 7: 282475249}
+    gLterm = [1, 2, 3, 4, 5, 6, 7]
+    for gL in gLterm:
+        if gL == 1:
+            total.append(outComesWithgLterm1())
+        else:
+            tempTotal = loopFunc(gL, 12, 10)
+            gLTotals.append([gL, tempTotal])
+            total.append(tempTotal * combBotTen[gL])
+
+    return total
+
+run()
